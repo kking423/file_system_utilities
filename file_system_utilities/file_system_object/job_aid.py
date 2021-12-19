43a0285865bbc1e01f9af3dd48557f652e38765c
Kyle King <kking423@gmail.com>
2021-12-19 07:34:31 -0500

    Add job aid

---
+++ file_system_utilities/file_system_object/job_aid.py
@@ -0,0 +1,18 @@
+import os
+import glob
+
+home = os.path.expanduser('~').replace('\\', '/')
+
+
+def get_latest_file():
+    latest_edited_folder = max(os.scandir(home), key=lambda x: x.stat().st_mtime).name
+    search_on = home + '/' + latest_edited_folder + '/*'
+
+    # return all file paths that match search pattern
+    list_of_files = glob.glob(search_on)
+
+    return max(list_of_files, key=os.path.getctime)
+
+
+def get_latest_files(from_date):
+   # TODO: get list of recently edited files from specified date
